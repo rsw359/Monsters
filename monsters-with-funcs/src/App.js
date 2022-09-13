f03717550cd44d6f422e3587da57416faf9fa58e
Roger Wells <rsw359@gmail.com>
2022-09-13 15:33:09 -0700

    app, cardlist, card, updated

--- monsters-with-funcs/src/App.js
+++ monsters-with-funcs/src/App.js
@@ -1,59 +1,98 @@
 import "./App.css";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 // import { UserData } from "./components/users-data/users-data.component";
 // import users from "./components/users-data/users.json";
-class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  componentDidMount() {
-    console.log(this.state);
+  console.log("render");
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    //was previously on line 52 as an anonymous function, but now updates the search field on render
-    const searchField = event.target.value.toLocaleLowerCase(); // lower case all search values in the event target
-
-    this.setState(() => {
-      return { searchField }; //update state to the new array
-    });
-  };
-  render() {
-    const { monsters, searchField } = this.state; //optimization for readability, this.state & this.~ not needed lines 45, 47, 56
-    const { onSearchChange } = this; //see #43
-
-    const filteredMonsters = monsters.filter((monster) => {
-      // filter out monsters that dont match the search string, filter returns an array
+  useEffect(() => {
+    const newfilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilteredMonsters(newfilteredMonsters);
+  }, [monsters, searchField]);
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters List</h1>
-        <SearchBox
-          className="monster-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="Search Monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase(); // lower case all search values in the event target
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters List</h1>
+      <SearchBox
+        className="monster-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="Search Monsters"
+      />
+
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     console.log(this.state);
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+
+//   onSearchChange = (event) => {
+//     //was previously on line 52 as an anonymous function, but now updates the search field on render
+//     const searchField = event.target.value.toLocaleLowerCase(); // lower case all search values in the event target
+
+//     this.setState(() => {
+//       return { searchField }; //update state to the new array
+//     });
+//   };
+//   render() {
+//     const { monsters, searchField } = this.state; //optimization for readability, this.state & this.~ not needed lines 45, 47, 56
+//     const { onSearchChange } = this; //see #43
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       // filter out monsters that dont match the search string, filter returns an array
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters List</h1>
+//         <SearchBox
+//           className="monster-search-box"
+//           onChangeHandler={onSearchChange}
+//           placeholder="Search Monsters"
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- monsters-with-funcs/src/components/card-list/card-list.component.jsx
+++ monsters-with-funcs/src/components/card-list/card-list.component.jsx
@@ -1,19 +1,12 @@
-import { Component } from "react";
 import "./card-list.styles.css";
 import Card from "../card/card.component";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster, index) => {
-          return <Card monster={monster} key={index} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster, index) => {
+      return <Card monster={monster} key={index} />;
+    })}
+  </div>
+);
 
 export default CardList;
